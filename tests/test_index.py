6f2702ad4ee2bff49bf69a01be0afed366a8542a
Rohan Mitchell <rohan@rohanmitchell.com>
2021-08-25 10:03:34 +1000

    Install BeautifulSoup to work with HTML in tests

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,3 +1,6 @@
+from bs4 import BeautifulSoup
+
 def test_index(client):
     rv = client.get('/')
-    assert b'Hello, world!' in rv.data
+    soup = BeautifulSoup(rv.data, 'html.parser')
+    assert soup.select('h1')[0].string == 'Hello, world!'

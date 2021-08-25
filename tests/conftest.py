b75706d68fccb76915c76bfcadbbeaf3ba2f7271
Rohan Mitchell <rohan@rohanmitchell.com>
2021-08-25 10:42:33 +1000

    Install pytest-flask

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,8 +3,4 @@ import pytest
 from app import create_app
 
 @pytest.fixture
-def client():
-    app = create_app({'TESTING': True})
-
-    with app.test_client() as client:
-        yield client
+def app(): return create_app()

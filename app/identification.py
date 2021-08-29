1b2f21bf94313184fb920ad88768b3898607174c
Rohan Mitchell <rohan@rohanmitchell.com>
2021-08-29 17:51:49 +1000

    Extract model to lib

--- app/identification.py
+++ app/identification.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask import request
 from flask import render_template
 
-from fastai.vision.all import *
+from app.lib.model import predict
 
 bp = Blueprint('identification', __name__)
 
@@ -11,29 +11,3 @@ def create():
     photo = request.files['photo']
     predictions = predict(photo.stream)
     return render_template('identification.html', predictions=predictions)
-
-
-def predict(photo):
-    learner = load_learner('models/model.pkl')
-    image = PILImage.create(photo)
-
-    _, _, outputs = learner.predict(image)
-    predictions = get_top_predictions(5, outputs, learner.dls.vocab)
-
-    return predictions
-
-def get_top_predictions(num_predictions, outputs, vocab):
-    top_classes = outputs.topk(num_predictions)
-
-    return [
-        {
-            'scientific_name': format_scientific_name(
-                vocab[top_classes.indices[i]],
-            ),
-            'confidence': round(top_classes.values[i].item()*100)
-        } for i in range(num_predictions)
-    ]
-
-
-def format_scientific_name(scientific_name):
-    return scientific_name.replace('_', ' ').capitalize()
